bbe9027a901d482b5beb54d6ab1606e5fca0163a
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-05-18 09:50:09 +0300

    login user tweak

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,12 +4,12 @@ import CustomInput from '../../components/shared/FormComponents/CustomInput';
 const Login = () => {
     return (
         <div className="flex flex-row 2xs:">
-            <div className="md:w-[50vw] md:h-[100vh] md:block 2xs:hidden md:bg-login-user bg-cover bg-left bg-no-repeat">
+            <div className="lg:w-[50vw] lg:h-[100vh] lg:block 2xs:hidden md:bg-login-user bg-cover bg-left bg-no-repeat">
                 <div className="bg-black bg-opacity-50 h-[100vh] flex flex-col justify-end pl-5 pb-5">
                     <div className="absolute top-10 left-5 bg-logo-join bg-cover h-[5rem] w-[6.2rem]"></div>
                 </div>
             </div>
-            <div className="flex flex-col justify-center items-center md:w-[50vw] 2xs:w-[100vw] md:mt-0 2xs:mt-[20%]">
+            <div className="flex flex-col justify-center items-center lg:w-[50vw] 2xs:w-[100vw] xl:mt-0 md:mt-[10%] 2xs:mt-[20%]">
                 <h1 className="text-4xl">Welcome</h1>
                 <p className="text-lg mb-5">Please enter your details below</p>
                 <button className="btn btn-wide bg-white text-gray-700 border-gray-500 mb-4">  sign up with google</button>
@@ -25,7 +25,8 @@ const Login = () => {
                     <span className='text-sm'>remember me</span>
                     <a href="" className='text-sm pl-5 text-cyan-600'>forgot password?</a>
                 </div>
-                <button className='btn btn-wide bg-black text-white hover:bg-white hover:text-black hover:border-none'>Log in</button>
+                <button className='btn btn-wide bg-black text-white hover:bg-white hover:text-black hover:border-none mb-3'>Log in</button>
+                <p className='text-sm'>Don’t have an account?<a href="" className='text-sm pl-1 text-cyan-600'>Sign up for FREE</a></p>
             </div>
         </div>
     );
