71325b56769a66592d9b56caedc2d36055500658
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-05-22 03:32:31 +0300

    🐛 Fixed vercel rendering images

--- src/containers/EachGovernorate/CheckDestenations/index.jsx
+++ src/containers/EachGovernorate/CheckDestenations/index.jsx
@@ -3,6 +3,11 @@ import { Link } from "react-router-dom";
 
 import CheckDestCard from "../../../components/CheckDestCard";
 
+import Image1 from "../../../assets/images/CheckDestinations/Aswan.png";
+import Image2 from "../../../assets/images/CheckDestinations/Luxor.png";
+import Image3 from "../../../assets/images/CheckDestinations/Nouba.png";
+import Image4 from "../../../assets/images/CheckDestinations/Alex.png";
+
 const CheckDestenations = () => {
   return (
     <section className="py-14">
@@ -17,22 +22,10 @@ const CheckDestenations = () => {
         </div>
 
         <div className="container grid gap-2 2xs:grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          <CheckDestCard
-            city="Aswan"
-            url="../../../../src/assets/images/CheckDestinations/Aswan.png"
-          />
-          <CheckDestCard
-            city="Luxor"
-            url="../../../../src/assets/images/CheckDestinations/Luxor.png"
-          />
-          <CheckDestCard
-            city="Nouba"
-            url="../../../../src/assets/images/CheckDestinations/Nouba.png"
-          />
-          <CheckDestCard
-            city="Alex"
-            url="../../../../src/assets/images/CheckDestinations/Alex.png"
-          />
+          <CheckDestCard city="Aswan" url={Image1} />
+          <CheckDestCard city="Luxor" url={Image2} />
+          <CheckDestCard city="Nouba" url={Image3} />
+          <CheckDestCard city="Alex" url={Image4} />
         </div>
       </div>
     </section>
