64856fc35cce052cbe191850a8cc79ba7c082d08
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-05-24 11:56:41 +0300

    :heavy_plus_sign: Accordion

--- src/components/shared/AccordionFAQ/index.jsx
+++ src/components/shared/AccordionFAQ/index.jsx
@@ -1,66 +1,61 @@
-/* eslint-disable react/prop-types */
-import { Fragment, useState } from "react";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from "@material-tailwind/react";
-function Icon({ id, open }) {
+import Pen  from '../../../assets/images/FAQ/Pen.svg';
+import { Collapse } from 'antd';
+const { Panel } = Collapse;
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+const AccordionFAQ = () => {
+  const onChange = (key) => {
+    console.log(key);
+  }
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className={`${
-        id === open ? "rotate-180" : ""
-      } h-5 w-5 transition-transform`}
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
-    </svg>
+    <>
+      <Collapse
+        defaultActiveKey={['1']}
+        onChange={onChange}
+        expandIconPosition='end'
+      >
+        <Panel header={
+        <div className='flex flex-row items-center'>
+        <img src={Pen}
+        className='h-7 w-7 mr-4'
+        onClick={(event) => {
+        event.stopPropagation();
+        }}/>
+        <span>This is panel header 1</span>
+        </div>
+        } key="1">
+          <div>{text}</div>
+        </Panel>
+        <Panel header={
+        <div className='flex flex-row items-center'>
+        <img src={Pen}
+        className='h-7 w-7 mr-4'
+        onClick={(event) => {
+        event.stopPropagation();
+        }}/>
+        <span>This is panel header 2</span>
+        </div>
+        } key="2">
+          <div>{text}</div>
+        </Panel>
+        <Panel header={
+        <div className='flex flex-row items-center'>
+        <img src={Pen}
+        className='h-7 w-7 mr-4'
+        onClick={(event) => {
+        event.stopPropagation();
+        }}/>
+        <span>This is panel header 3</span>
+        </div>
+        } key="3">
+          <div>{text}</div>
+        </Panel>
+      </Collapse>
+      <br />
+    </>
   );
-}
-export default function Example() {
-  const [open, setOpen] = useState(0);
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
-  return (
-    <Fragment>
-      <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(1)}>
-          What is Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(2)}>
-          How to use Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-      <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
-        <AccordionHeader onClick={() => handleOpen(3)}>
-          What can I do with Material Tailwind?
-        </AccordionHeader>
-        <AccordionBody>
-          We&apos;re not always in the position that we want to be at.
-          We&apos;re constantly growing. We&apos;re constantly making mistakes.
-          We&apos;re constantly trying to express ourselves and actualize our
-          dreams.
-        </AccordionBody>
-      </Accordion>
-    </Fragment>
-  );
-}
+};
+export default AccordionFAQ;
---
+++ src/pages/FAQ/index.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+import Navbar from '../../components/shared/Navbar/index';
+import AccordionFAQ from "../../components/shared/AccordionFAQ";
+
+const Faq = () => {
+    return (
+        <>
+            <Navbar/>
+            <AccordionFAQ/>
+        </>
+    );
+}
+export default Faq;
