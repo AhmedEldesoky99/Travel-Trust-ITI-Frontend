ff1f9e057f8730c35ddd219a303e0461d763f0b2
KMeniawy <karim.elmeniawy.99@gmail.com>
2023-06-05 08:49:41 +0300

    responsive cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -41,8 +41,8 @@ const Cart = () => {
               {tourCards}
             </div>
           </div>
-          <div className="border border-gray-200 max-w-[405px] max-h-[413px] w-full h-full flex flex-col gap-2 justify-center p-6 rounded-2xl">
-            <p className="text-2xl mb-6 mt-6">Order Summary</p>
+          <div className="md:border-2 max-w-[405px] max-h-[413px] w-full h-full flex flex-col gap-2 justify-center md:p-6 rounded-2xl">
+            <p className="text-2xl my-6">Order Summary</p>
             <div className="flex justify-between text-base">
               <p>booking fee</p>
               <p>0.00 EGP</p>
