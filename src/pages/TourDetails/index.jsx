9d21caa59ae974e152e509e2afeef68c69ce3abc
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-06-20 14:14:58 +0300

    ⚡ ️Tour Details v1

--- src/pages/TourDetails/index.jsx
+++ src/pages/TourDetails/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import Header from "../../containers/TourDetails/Header";
 import TourTabs from "../../containers/TourDetails/TourTabs";
 
+import { useTour } from "../../services/useTour";
+
 const TourDetails = () => {
+  const { id } = useParams();
+  const { getTourDetails } = useTour();
+  const { data, isLoading, isSuccess, isError } = getTourDetails(id);
+
   return (
     <>
-      <Header />
-      <TourTabs />
+      <Header data={data} isLoading={isLoading} />
+      <TourTabs data={data} isLoading={isLoading} />
     </>
   );
 };
