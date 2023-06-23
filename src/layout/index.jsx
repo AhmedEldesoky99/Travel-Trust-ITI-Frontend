52f6536b1ecf72100de4f0dba129cf439473f83f
Ahmed Eldesoky <61599746+AhmedEldesoky99@users.noreply.github.com>
2023-06-23 18:43:39 +0300

    Update index.jsx

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -9,6 +9,11 @@ import { UserIdProvider } from "../context/UserIdContext";
 
 const Layout = () => {
   const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
+  
   const pathsToHide = [
     "/join",
     "/login",
